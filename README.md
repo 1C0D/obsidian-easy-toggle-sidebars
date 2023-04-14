00b0acecfa54091822518fb6523bf68ed66f0283
1C0D <63018905+1C0D@users.noreply.github.com>
2023-04-14 10:37:41 +0200

    command 'Toggle both sidebars' added

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Easy toggle sidebars 
   
-This plugin allows the user to toggle the sidebars in a workspace by double-clicking the middle mouse button. It also allows the user to open and close each sidebar by clicking the middle mouse button and moving the mouse towards the corresponding sidebar.  
+This plugin allows the user to toggle the sidebars by double-clicking the middle mouse button. It also allows the user to open and close each sidebar by clicking the middle mouse button and moving the mouse towards the corresponding sidebar.  
+Edit: You can now add your own shortcut to the command "Toggle both sidebars"
+N.B: Shortcuts are natively possible to toggle each sidebar
   
 ![demo](toggle-panels.gif)
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
-import {
-	Plugin,
-	WorkspaceSidedock,
-} from "obsidian";
+import { Plugin, WorkspaceSidedock } from "obsidian";
 
 export default class EasytoggleSidebar extends Plugin {
 	leftSplit: WorkspaceSidedock;
@@ -12,48 +9,52 @@ export default class EasytoggleSidebar extends Plugin {
 			let startX = 0;
 			let threshold = 150;
 
-			this.registerDomEvent(
-				document,
-				"mousedown",
-				(evt: any) => {
-					if (evt.button !== 1 ) {
-						return;
-					} else {
-						startX = evt.clientX;
-					}
+			this.registerDomEvent(document, "mousedown", (evt: any) => {
+				if (evt.button !== 1) {
+					return;
+				} else {
+					startX = evt.clientX;
 				}
-			);
+			});
 
-			this.registerDomEvent(
-				document,
-				"mouseup",
-				(evt: any) => {
-					if (evt.button === 1 && evt.detail === 1) {
-						let endX = evt.clientX;
-						let distance = Math.sqrt(Math.pow(endX - startX, 2));
-						if (distance > threshold) {
-							if (endX < startX) {
-								this.toggle(this.getLeftSplit());
-							} else {
-								this.toggle(this.getRightSplit());
-							}
-						}
-					}
-					if (evt.button === 1 && evt.detail === 2) {
-						const isLeftOpen = this.isOpen(this.getLeftSplit());
-						const isRightOpen = this.isOpen(this.getRightSplit());
-						if (isLeftOpen && !isRightOpen) {
+			this.registerDomEvent(document, "mouseup", (evt: any) => {
+				if (evt.button === 1 && evt.detail === 1) {
+					let endX = evt.clientX;
+					let distance = Math.sqrt(Math.pow(endX - startX, 2));
+					if (distance > threshold) {
+						if (endX < startX) {
 							this.toggle(this.getLeftSplit());
-						} else if (isRightOpen && !isLeftOpen) {
-							this.toggle(this.getRightSplit());
 						} else {
-							this.toggle(this.getLeftSplit());
 							this.toggle(this.getRightSplit());
 						}
 					}
 				}
-			);
+				if (evt.button === 1 && evt.detail === 2) {
+					this.toggleBothSidebars();
+				}
+			});
+
+			this.addCommand({
+				id: "toggle-both-sidebars",
+				name: "Toggle both sidebars",
+				callback: () => {
+					this.toggleBothSidebars();
+				},
+			});
 		});
+	}
+
+	toggleBothSidebars() {
+		const isLeftOpen = this.isOpen(this.getLeftSplit());
+		const isRightOpen = this.isOpen(this.getRightSplit());
+		if (isLeftOpen && !isRightOpen) {
+			this.toggle(this.getLeftSplit());
+		} else if (isRightOpen && !isLeftOpen) {
+			this.toggle(this.getRightSplit());
+		} else {
+			this.toggle(this.getLeftSplit());
+			this.toggle(this.getRightSplit());
+		}
 	}
 
 	getLeftSplit(): WorkspaceSidedock {
